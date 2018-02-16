c123b574eb2195e81fa1957229a1ff2ae1236c37
Jan Klemkow <j.klemkow@wemelug.de>
2018-02-17 00:08:43 +0100

    simplify ethernet handling

--- eth2.c
+++ eth2.c
@@ -1,31 +1,25 @@
 #include <sys/types.h>
 #include <stdio.h>
 
-void
-print_dst_mac(const u_char *data, int offset)
-{
-	printf("\"dest_mac, %02x:%02x:%02x:%02x:%02x:%02x\",",
-	    data[offset], data[offset+1], data[offset+2],
-	    data[offset+3], data[offset+4], data[offset+5]);
-}
-void
-print_src_mac(const u_char *data, int offset)
-{
-	printf("\"src_mac, %02x:%02x:%02x:%02x:%02x:%02x\",",
-	    data[offset+6], data[offset+7], data[offset+8],
-	    data[offset+9], data[offset+10], data[offset+11]);
-}
-
-void
-print_ether_type(const u_char *data, int offset)
-{
-	printf("\"ether_type, 0x%02x%02x\",", data[offset+12], data[offset+13]);
-}
+struct frame {
+	uint8_t dst[6];
+	uint8_t src[6];
+	/* TODO: handl optional Q-Tag */
+	uint32_t type;
+};
 
 void
 print_ether_fields(const u_char *data, int offset)
 {
-	print_dst_mac(data, offset);
-	print_src_mac(data, offset);
-	print_ether_type(data, offset);
+	struct frame *frame = (struct frame *)(data + offset);
+
+	printf("\"dest_mac, %02x:%02x:%02x:%02x:%02x:%02x\",",
+	    frame->dst[0], frame->dst[1], frame->dst[2],
+	    frame->dst[3], frame->dst[4], frame->dst[5]);
+
+	printf("\"src_mac, %02x:%02x:%02x:%02x:%02x:%02x\",",
+	    frame->src[0], frame->src[1], frame->src[2],
+	    frame->src[3], frame->src[4], frame->src[5]);
+
+	printf("\"ether_type, 0x%04x\",", frame->type);
 }
