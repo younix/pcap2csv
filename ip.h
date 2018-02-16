68c5227f2676f4fa9746ae119616348136fbcb16
Jan Klemkow <j.klemkow@wemelug.de>
2018-02-16 22:27:08 +0100

    spacing

--- ip.h
+++ ip.h
@@ -1,20 +1,20 @@
 #ifndef IP_H
 #define IP_H
 
-void print_version_ip(const u_char * data, int offset);
-void print_ihl(const u_char * data, int offset);
-int get_ihl(const u_char * data, int offset);
-void print_tos_ip(const u_char * data, int offset);
-void print_tot_len(const u_char * data, int offset);
-int get_tot_len(const u_char * data, int offset);
-void print_id_ip(const u_char * data, int offset);
-void print_flags_ip(const u_char * data, int offset);
-void print_frag_off(const u_char * data, int offset);
-void print_ttl_ip(const u_char * data, int offset);
-void print_protocol(const u_char * data, int offset);
-void print_chksum(const u_char * data, int offset);
-void print_src_ip_addr(const u_char * data, int offset);
-void print_dst_ip_addr(const u_char * data, int offset);
-void print_ip_fields(const u_char * data, int offset);
+void print_version_ip(const u_char *data, int offset);
+void print_ihl(const u_char *data, int offset);
+int get_ihl(const u_char *data, int offset);
+void print_tos_ip(const u_char *data, int offset);
+void print_tot_len(const u_char *data, int offset);
+int get_tot_len(const u_char *data, int offset);
+void print_id_ip(const u_char *data, int offset);
+void print_flags_ip(const u_char *data, int offset);
+void print_frag_off(const u_char *data, int offset);
+void print_ttl_ip(const u_char *data, int offset);
+void print_protocol(const u_char *data, int offset);
+void print_chksum(const u_char *data, int offset);
+void print_src_ip_addr(const u_char *data, int offset);
+void print_dst_ip_addr(const u_char *data, int offset);
+void print_ip_fields(const u_char *data, int offset);
 
 #endif
